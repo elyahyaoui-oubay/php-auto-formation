589bce5a76eb28ec9c7c578f129ea22ea2082510
elyahyaoui-oubay <elyahyaoui.oubay@gmail.com>
2022-01-26 16:01:28 +0100

    add exemples de variable

---
+++ Variable Types/exemple1.php
@@ -0,0 +1,11 @@
+Saltus les gens 
+
+<?php
+$prenom = 'Oubay';
+$nom = 'El Yahyaoui';
+echo $prenom . ' ' . $nom;
+
+?>
+
+Comment ça va?
+
---
+++ Variable Types/exemple2.php
@@ -0,0 +1,10 @@
+<?php
+$prenom = 'Oubay';
+$nom = 'El Yahyaoui';
+$note1 = 10;
+$note2 = 20;
+$moyenne = ($note1 + $note2) / 2;
+
+echo 'Bonjour' .$prenom . ' ' . $nom . 'vous avez eu' . (($note1 + $note2) / 2) . 'de moyenne';
+
+echo "\nBonjour $prenom $nom vous aves eu $moyenne de moyenne";
